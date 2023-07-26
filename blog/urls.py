979240e4546d1b87c7fde2c03047fe0ec54aa6a0
Ventel1555 <ventelorp@gmail.com>
2023-07-26 21:11:06 +0300

    fix small bug

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('posts/<slug:slug>/',views.post_detail, name='post_detail'),
     path('',views.post_list, name='post_list'),
-    path('posts/new/', views.post_new, name='post_new'),
+    path('post/new/', views.post_new, name='post_new'),
     path('posts/<slug:slug>/edit/', views.post_edit, name='post_edit'),
-    path('post/<slug:slug>/delete/', views.BlogDeleteView.as_view(), name='post_delete'),
+    path('posts/<slug:slug>/delete/', views.BlogDeleteView.as_view(), name='post_delete'),
 ]
