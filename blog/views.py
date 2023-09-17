60b294e0e1bdd21f0c0e0af933a7c52a29439656
MironBerch <106734953+MironBerch@users.noreply.github.com>
2023-09-17 16:17:34 +0300

    Update views.py
    Edit links

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
 from django.core.paginator import Paginator
-
 from django.urls import reverse_lazy
-
 from django.views.generic.edit import DeleteView
-
 from django.contrib.auth.decorators import login_required
 
 from blog.utils import AuthenticatedMixin
-
 from .models import Post
-
 from .forms import CommentForm, PostForm
 
 
